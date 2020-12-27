bfc656f89f2ce0caf566a7f700daa75fbbf99dcb
bryan cho <bryancho31@gmail.com>
2020-12-26 22:51:12 -0600

    프로젝트 초기화

---
+++ DotNetSaleCore.WebForms.Tests/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetSaleCore.WebForms.Tests.Startup))]
+namespace DotNetSaleCore.WebForms.Tests
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
